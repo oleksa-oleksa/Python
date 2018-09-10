284fbe9706ffe0eec4f8beeefed4a81c8b03d947
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-09-10 17:27:33 +0200

    using max

--- poker.py
+++ poker.py
@@ -1,3 +1,16 @@
+# -----------
+# User Instructions
+# 
+# Modify the poker() function to return the best hand 
+# according to the hand_rank() function. Since we have
+# not yet written hand_rank(), clicking RUN won't do 
+# anything, but clicking SUBMIT will let you know if you
+# have gotten the problem right. 
+#
+
 def poker(hands):
-	"Return the best hand: poker([hand,...]) => hand"
-	return ??
+    "Return the best hand: poker([hand,...]) => hand"
+    return max(hands, key = hand_rank)
+
+def hand_rank(hand):
+    return None # we will be changing this later.

5492dd0c624010b2f303e8c7f5e7ab0f67e210bb
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-09-10 17:28:24 +0200

    Testing

--- poker.py
+++ poker.py
@@ -14,3 +14,18 @@ def poker(hands):
 
 def hand_rank(hand):
     return None # we will be changing this later.
+
+
+def test():
+    "Test cases for the functions in poker program"
+    sf = "6C 7C 8C 9C TC".split() # => ['6C', '7C', '8C', '9C', 'TC']
+    fk = "9D 9H 9S 9C 7D".split() 
+    fh = "TD TC TH 7C 7D".split()
+    assert poker([sf, fk, fh]) == sf
+    
+    # Add 2 new assert statements here. The first 
+    # should check that when fk plays fh, fk 
+    # is the winner. The second should confirm that
+    # fh playing against fh returns fh.
+    
+print test()

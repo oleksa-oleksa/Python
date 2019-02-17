e5a8e3266e505f823cbb91ddbe354a9035402402
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-02-17 17:28:55 +0100

    rename files lesson 2

--- Programming_Foundations_with_Python_Udacity/rename_files.py
+++ Programming_Foundations_with_Python_Udacity/rename_files.py
@@ -1,4 +1,18 @@
-def rename_files():
-    #1. get file names from the folder
+import os
 
-    #2. for each file, rename the filename
+def rename_files():
+    # 1. get file names from the folder
+    file_list = os.listdir("img")
+    print(file_list)
+    saved_path = os.getcwd()
+    print("Started at " + saved_path)
+    os.chdir("./img")
+    cur_path = os.getcwd()
+    print("Current folders " + cur_path)
+
+    # 2. for each file, rename the filename
+    for file_name in file_list:
+        os.rename(file_name, (file_name.translate(None, "0123456789")))
+    os.chdir(saved_path)
+
+rename_files()

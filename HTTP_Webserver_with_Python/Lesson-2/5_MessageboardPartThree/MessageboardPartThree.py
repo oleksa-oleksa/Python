e43b8d6e013076a3acc92b7b74c842bc55209e6f
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-04-18 15:35:56 +0200

    post-redirect-get

--- HTTP_Webserver_with_Python/Lesson-2/5_MessageboardPartThree/MessageboardPartThree.py
+++ HTTP_Webserver_with_Python/Lesson-2/5_MessageboardPartThree/MessageboardPartThree.py
@@ -41,6 +41,9 @@ class MessageHandler(BaseHTTPRequestHandler):
         memory.append(message)
 
         # 1. Send a 303 redirect back to the root page.
+        self.send_response(303)
+        self.send_header('Location', '/')
+        self.end_headers()
 
     def do_GET(self):
         # First, send a 200 OK response.
@@ -51,8 +54,9 @@ class MessageHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # 2. Put the response together out of the form and the stored messages.
-
+        mesg = form.format("\n".join(memory))
         # 3. Send the response.
+        self.wfile.write(mesg.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)

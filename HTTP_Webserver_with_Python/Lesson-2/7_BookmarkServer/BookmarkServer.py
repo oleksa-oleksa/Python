f313a699bc1b64017367e6d3413bf51f664a72f4
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-06 15:42:10 +0200

    Lesson2 Bookmark server

--- HTTP_Webserver_with_Python/Lesson-2/7_BookmarkServer/BookmarkServer.py
+++ HTTP_Webserver_with_Python/Lesson-2/7_BookmarkServer/BookmarkServer.py
@@ -93,7 +93,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 self.send_response(303)
                 self.send_header('Location', memory[name])
                 self.end_headers()
-                
+
             else:
                 # We don't know that name! Send a 404 error.
                 self.send_response(404)
@@ -117,10 +117,17 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         params = parse_qs(body)
 
         # Check that the user submitted the form fields.
-        if "longuri" not in params or "shortname" not in params:
-            # 3. Serve a 400 error with a useful message.
-            #    Delete the following line.
-            raise NotImplementedError("Step 3 isn't written yet!")
+        if "longuri" not in params:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Please provide longURI".encode())
+
+        elif "shortname" not in params:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Please provide short name".encode())
 
         longuri = params["longuri"][0]
         shortname = params["shortname"][0]
@@ -130,14 +137,16 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             memory[shortname] = longuri
 
             # 4. Serve a redirect to the root page (the form).
-            #    Delete the following line.
-            raise NotImplementedError("Step 4 isn't written yet!")
+            self.send_response(303)
+            self.send_header('Location', '/')
+            self.end_headers()
+
         else:
             # Didn't successfully fetch the long URI.
-
-            # 5. Send a 404 error with a useful message.
-            #    Delete the following line.
-            raise NotImplementedError("Step 5 isn't written yet!")
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)

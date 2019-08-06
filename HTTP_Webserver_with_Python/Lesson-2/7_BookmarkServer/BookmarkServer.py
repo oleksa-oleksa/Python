fac5566f5fac647e833097ea4998f623d5f55ae4
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-06 15:15:18 +0200

    Lesson2 Bookmark server

--- HTTP_Webserver_with_Python/Lesson-2/7_BookmarkServer/BookmarkServer.py
+++ HTTP_Webserver_with_Python/Lesson-2/7_BookmarkServer/BookmarkServer.py
@@ -91,9 +91,9 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             if name in memory:
                 # 2. Send a 303 redirect to the long URI in memory[name].
                 self.send_response(303)
-                self.send_header('Location', memory.get('name', '/'))
-                #    Delete the following line.
-                raise NotImplementedError("Step 2 isn't written yet.")
+                self.send_header('Location', memory[name])
+                self.end_headers()
+                
             else:
                 # We don't know that name! Send a 404 error.
                 self.send_response(404)

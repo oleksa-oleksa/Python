fd6c48e6c504a14a939175d2c091487c8a9ed2de
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-08 18:38:09 +0200

    GaussianNB

--- Machine_Learning_ud120/gaussianNB.py
+++ Machine_Learning_ud120/gaussianNB.py
@@ -1,4 +1,10 @@
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-clf.fit(features, labels)
-pred = clf.predict(predict_set)
+from sklearn.metrics import accuracy_score
+
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    pred = clf.predict(features_test)
+    accuracy = accuracy_score(pred, features_test)
+    print(accuracy)
+    return accuracy

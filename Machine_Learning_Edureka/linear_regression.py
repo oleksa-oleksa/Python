88d2d87d74db3e32415b8e5cb167694655f132ae
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-12 16:17:51 +0200

    gitignore modified, linear regrisson lesson continued

--- Machine_Learning_Edureka/linear_regression.py
+++ Machine_Learning_Edureka/linear_regression.py
@@ -1,9 +1,20 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
-%matplotlib inline
 boston = pd.read_csv("/datasets/BostonHousing.csv")
 
 # setting the dependent und independent variables
 x = boston.iloc[:,0:13]
 y = boston["medv"]
+
+# Compute pairwise correlation of columns, excluding NA/null values.
+names = []
+correlations = boston.corr()
+
+# Plot rectangular data as a color-encoded matrix.
+sns.heatmap(correlations, square=True, cmap="YiGnBu")
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+plt.show()
+

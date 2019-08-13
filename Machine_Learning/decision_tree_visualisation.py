9ba5cb4ed4b3c2f7863f1f66efc50bd9e6c329e6
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-13 13:15:59 +0200

    decision tree visualised

--- Machine_Learning/decision_tree_visualisation.py
+++ Machine_Learning/decision_tree_visualisation.py
@@ -7,15 +7,30 @@ Visualise the tree
 
 import pandas as pd
 from sklearn import tree
+import graphviz
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
-iris = pd.read_csv("./datasets/iris_train.csv")
-headers = iris.columns.tolist()
-print(headers)
+
+iris = pd.read_csv("./datasets/iris.csv")
 
 # setting the dependent und independent variables
+x = iris.iloc[:, 0:4].astype(float)
+y = iris["variety"]
 
 
-# The data will not be split because the separate dataset for train and test were provided
+print(x)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.35, random_state=10)
 
 clf = tree.DecisionTreeClassifier()
-#clf.fit(iris)
+clf = clf.fit(x_train, y_train)
+
+# prediction for a new flowers
+y_pred = clf.predict(x_test)
+
+dot_data = tree.export_graphviz(clf, out_file=None, feature_names=x_test.columns.values,
+                                class_names=y_test.values,
+                                filled=True, rounded=True, special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render("./export/iris")

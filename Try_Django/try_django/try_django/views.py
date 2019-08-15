1eed15f865bb316292abf155c63b3badb4ff5df6
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-15 11:31:49 +0200

    django template index.html

--- Try_Django/try_django/try_django/views.py
+++ Try_Django/try_django/try_django/views.py
@@ -1,9 +1,10 @@
 # Model View Template
 # Whatever we want to change a page we start with a view
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<h1>Hello!</h1>")
+    return render(request, "index.html")
 
 
 def about_page(request):

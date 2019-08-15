8f94553a13c595eba933f260ae733f8e7b4fd9b8
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-15 10:58:51 +0200

    django view

--- Try_Django/try_django/try_django/views.py
+++ Try_Django/try_django/try_django/views.py
@@ -2,7 +2,15 @@
 # Whatever we want to change a page we start with a view
 from django.http import HttpResponse
 
-def home_page():
+def home_page(request):
     return HttpResponse("<h1>Hello!</h1>")
+
+
+def about_page(request):
+    return HttpResponse("<h1>I am Oleksa</h1>")
+
+
+def contact_page(request):
+    return HttpResponse("<h1>Write me and hire me! Give me money!</h1>")
 
 

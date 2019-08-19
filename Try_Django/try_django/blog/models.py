921864cff892301b571f99dce830b9590f134793
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-19 10:59:19 +0200

    starting django application

--- Try_Django/try_django/blog/models.py
+++ Try_Django/try_django/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.TextField()
+    conten = models.TextField(null=True, blank=True)
+
+
+
+class Blog:
+    title = "Hello world"
+    content = "something cool"

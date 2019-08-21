64b1eb568deea85ca111f265bf7793de1cb7bcbb
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-21 11:21:28 +0200

    Migrations

--- Try_Django/try_django/blog/models.py
+++ Try_Django/try_django/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class BlogPost(models.Model):
     title = models.TextField()
-    conten = models.TextField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
 
 
 

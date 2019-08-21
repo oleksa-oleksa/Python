a839e48316d20d72c022c13818f85299d372d893
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-21 11:52:31 +0200

    post detail page

--- Try_Django/try_django/blog/views.py
+++ Try_Django/try_django/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import BlogPost
+
+def blog_post_detail_page(request):
+    obj = BlogPost.objects.get(id=1)
+    template_name = "blog_post_detail.html"
+    context = {"object": obj}
+    return render(request, template_name, context)

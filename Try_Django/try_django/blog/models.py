225e66c7df536c68fb84319eac18125d8b0bd62e
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-21 14:27:54 +0200

    post detail page

--- Try_Django/try_django/blog/models.py
+++ Try_Django/try_django/blog/models.py
@@ -5,8 +5,6 @@ class BlogPost(models.Model):
     title = models.TextField()
     content = models.TextField(null=True, blank=True)
 
-
-
 class Blog:
     title = "Hello world"
     content = "something cool"
--- Try_Django/try_django/blog/views.py
+++ Try_Django/try_django/blog/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from .models import BlogPost
 
 def blog_post_detail_page(request):
-    obj = BlogPost.objects.get(id=1)
+    obj = BlogPost.objects.get(id=2)
     template_name = "blog_post_detail.html"
     context = {"object": obj}
     return render(request, template_name, context)
+

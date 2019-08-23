3cdf57f6b454f010caa9f8898aeecc3c4c087abc
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-23 12:12:00 +0200

    raise 404

--- Try_Django/try_django/blog/views.py
+++ Try_Django/try_django/blog/views.py
@@ -1,10 +1,14 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 from .models import BlogPost
 
 def blog_post_detail_page(request, post_id):
-    obj = BlogPost.objects.get(id=post_id)
+    try:
+        obj = BlogPost.objects.get(id=post_id)
+    except:
+        raise Http404
     template_name = "blog_post_detail.html"
     context = {"object": obj}
     return render(request, template_name, context)

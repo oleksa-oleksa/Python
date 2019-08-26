986952456d47b3d9c8b6d112aba459e2f0e8c34e
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-26 14:49:53 +0200

    delete blog post

--- Try_Django/try_django/blog/views.py
+++ Try_Django/try_django/blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .models import BlogPost
@@ -53,9 +53,13 @@ def blog_post_update_view(request, slug):
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog/delete.html"
+    template_name = 'blog/delete.html'
+    if request.method == "POST":
+        obj.delete()
+        return redirect("/blog")
     context = {"object": obj}
     return render(request, template_name, context)
 

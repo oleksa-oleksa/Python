dc76eeb1ad27731f791a6676c02920dc86f7af20
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-26 13:44:07 +0200

    BlogPostForm validation

--- Try_Django/try_django/try_django/forms.py
+++ Try_Django/try_django/try_django/forms.py
@@ -6,6 +6,17 @@ class ContactForm(forms.Form):
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
 
+    ''' The clean_<fieldname>() method is called on a form subclass – where <fieldname> 
+    is replaced with the name of the form field attribute. This method does any cleaning 
+    that is specific to that particular attribute, unrelated to the type of field that it is. 
+    This method is not passed any parameters. '''
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        print(email)
+        if email.endswith(".edu"):
+            raise forms.ValidationError("This is not a valid email. Please don't use .edu.")
+        return email
+
 
 
 

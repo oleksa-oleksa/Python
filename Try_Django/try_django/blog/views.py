fd35191b2573f5a772d7d4b7298b90d37c059a26
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-26 13:55:14 +0200

    Decorator for login_required

--- Try_Django/try_django/blog/views.py
+++ Try_Django/try_django/blog/views.py
@@ -1,4 +1,5 @@
-from django.http import Http404
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -17,6 +18,7 @@ def blog_post_list_view(request):
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_create_view(request):
     # create objects -> use a form
     form = BlogPostModelForm(request.POST or None)

5e3d5aec2692dec86ac4f95e82ee3db65d9f0246
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-26 15:05:01 +0200

    blog navigation

--- Try_Django/try_django/blog/models.py
+++ Try_Django/try_django/blog/models.py
@@ -11,3 +11,12 @@ class BlogPost(models.Model): # blogpost_set -> queryset
     title = models.CharField(max_length=150)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+
+    def get_edit_url(self):
+        return f"{self.get_absolute_url()}/edit"
+
+    def get_delete_url(self):
+        return f"{self.get_absolute_url()}/delete"

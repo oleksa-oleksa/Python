ef372164b27b64435e4dc215d152657fd519c542
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-26 14:02:11 +0200

    foreign key

--- Try_Django/try_django/blog/models.py
+++ Try_Django/try_django/blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
 class BlogPost(models.Model):
     # id = PrimaryKey, Integer
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=150)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)

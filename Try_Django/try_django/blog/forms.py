c59c7af1f8c9fd681c607fe715c22a3a274bd97e
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-26 13:48:59 +0200

    BlogPostForm title exist validation

--- Try_Django/try_django/blog/forms.py
+++ Try_Django/try_django/blog/forms.py
@@ -18,7 +18,9 @@ class BlogPostModelForm(forms.ModelForm):
     def clean_title(self, *args, **kwargs):
         instance = self.instance
         title = self.cleaned_data.get('title')
-        qs = BlogPost.objects.filter(title=title)
+        qs = BlogPost.objects.filter(title__iexact=title)
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk)  # id=instance.id
         if qs.exists():
             raise forms.ValidationError("This title has already been used. Please try again.")
         return title

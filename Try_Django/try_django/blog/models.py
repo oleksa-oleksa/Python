51c3bc8f54b6d65a78f15c09ff5ec3563ad6eef8
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-08-28 15:26:42 +0200

    static media url added

--- Try_Django/try_django/blog/models.py
+++ Try_Django/try_django/blog/models.py
@@ -21,6 +21,7 @@ class BlogPostManager(models.Manager):
 class BlogPost(models.Model): # blogpost_set -> queryset
     # id = PrimaryKey, Integer
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+    image = models.FileField(upload_to='image/', blank=True, null=True)
     title = models.CharField(max_length=150)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
--- Try_Django/try_django/try_django/urls.py
+++ Try_Django/try_django/try_django/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, re_path, include
 
 from blog.views import (
@@ -35,3 +36,9 @@ urlpatterns = [
     path('contact/', contact_page),
     path('admin/', admin.site.urls)
 ]
+
+if settings.DEBUG:
+    # test mode
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

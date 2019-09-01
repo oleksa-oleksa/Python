fbe392926d96534e24ee06032301c8f7809a22d1
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-09-01 12:14:20 +0200

    search query

--- Try_Django/try_django/search/views.py
+++ Try_Django/try_django/search/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import SearchQuery
 
 
 # Create your views here.
 def search_view(request):
-    return render(request, 'searches/view.html', {})
+    query = request.GET.get('q', None)
+    user = None
+    if request.user.is_authenticated:
+        user = request.user
+    context = {"query": query}
+
+    return render(request, 'searches/view.html', context)

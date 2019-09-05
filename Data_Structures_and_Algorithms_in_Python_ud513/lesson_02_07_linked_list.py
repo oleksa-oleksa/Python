ebab9b93b0fcb072803348c9103747de1a1ccd4b
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-09-05 11:36:58 +0200

    Linked list

---
+++ Data_Structures_and_Algorithms_in_Python_ud513/lesson_02_07_linked_list.py
@@ -0,0 +1,19 @@
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+

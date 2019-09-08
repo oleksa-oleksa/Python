9cef818cb99b6f280e6e76ba302e63c41993bfbd
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-09-08 11:53:10 +0200

    Stack delete, push, pop

--- Data_Structures_and_Algorithms_in_Python_ud513/lesson_02_10_stack.py
+++ Data_Structures_and_Algorithms_in_Python_ud513/lesson_02_10_stack.py
@@ -34,20 +34,22 @@ class LinkedList(object):
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        top = self.head
+        if self.head.next:
+            self.head = self.head.next
+        return top
 
+    class Stack(object):
+        def __init__(self, top=None):
+            self.ll = LinkedList(top)
 
-class Stack(object):
-    def __init__(self, top=None):
-        self.ll = LinkedList(top)
+        def push(self, new_element):
+            "Push (add) a new element onto the top of the stack"
+            self.ll.insert_first(new_element)
 
-    def push(self, new_element):
-        "Push (add) a new element onto the top of the stack"
-        pass
-
-    def pop(self):
-        "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        def pop(self):
+            "Pop (remove) the first element off the top of the stack and return it"
+            return self.ll.delete_first()
 
 
 # Test cases

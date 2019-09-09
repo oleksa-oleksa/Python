12ac791d94b542d715c488ff1c89d8f88a074223
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-09-09 15:29:36 +0200

    recursion translated to python

--- Data_Structures_and_Algorithms_in_Python_ud513/lesson_03_05_recursion.py
+++ Data_Structures_and_Algorithms_in_Python_ud513/lesson_03_05_recursion.py
@@ -1,13 +1,15 @@
-function getFib(position) {
-  if (position == 0) { return 0; }
-  if (position == 1) { return 1; }
-  var first = 0,
-      second = 1,
-      next = first + second;
-  for (var i = 2; i < position; i++) {
-    first = second;
-    second = next;
-    next = first + second;
-  }
-  return next;
-}
+def get_fib_loop(position):
+    if position == 0:
+        return 0
+    if position == 1:
+        return 1
+    first = 0
+    second = 1
+    nextt = first + second
+    i = 0
+    while i < position:
+        first = second
+        second = next
+        nextt = first + second
+        i += 1
+    return nextt

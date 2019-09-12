e91da63f74c957efd5c0374822f8e1585751dde9
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-09-12 16:15:29 +0200

    Machine Learning: Bayes Rule

---
+++ Machine_Learning/bayes_rule.py
@@ -0,0 +1,9 @@
+"""
+Example: Specific cancer that occurs to 1% of population P(c) = 0.01
+Test for this cancer with a 90% chance is positive if you have this cancer (this is sensitivity of the test)
+But the test is sometimes is positive even if you don't have a cancer C.
+Let's say with another 90% chance is negative if you don't have cancer C (this is specificity of the test)
+
+Question: the prior probability of cancer is 1%, and a sensitivity and specificity are 90%,
+what's the probability that someone with a positive cancer test actually has the disease?
+"""

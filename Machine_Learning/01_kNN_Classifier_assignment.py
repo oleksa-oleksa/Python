cd60ff5f0b121a33330d174da8870cbfe93173b1
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-09-15 15:19:08 +0200

    kNN Classifier assignment 1

---
+++ Machine_Learning/01_kNN_Classifier_assignment.py
@@ -0,0 +1,12 @@
+"""
+Aufgabe 1. Implementierung eines k-NN-Classifiers
+Implementieren Sie einen k-NN-Classifier in Python (inkl. numpy, pandas, matplotlib)
+auf der Jupyter Notebook-Umgebung. Nutzen Sie den „ZIP code“-Datensatz mit den Trainings- daten als Referenz
+für die Nachbarschaft. Testen Sie auf den Testdaten.
+
+Geben Sie die Konfusionsmatrix und die Klassifikationsgenauigkeit aus.
+
+Geben Sie mithilfe von Matplotlib ein paar der falsch klassifizierten Zahlen aus.
+Testen Sie, bei welchem k ist der Klassifizierer am besten ist.
+Was sind Vor- und Nachteile des k-NN-Classifiers?
+"""

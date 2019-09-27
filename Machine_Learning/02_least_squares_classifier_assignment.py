9da2624a1d3b64f39592e202ae8abac41536bcdd
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-09-27 15:38:02 +0200

    Linear regression assignment 2 problem with index in prediction list

--- Machine_Learning/02_least_squares_classifier_assignment.py
+++ Machine_Learning/02_least_squares_classifier_assignment.py
@@ -25,7 +25,7 @@ class Classifier:
         size = len(set(labels))
         matrix = np.zeros((size, size))
         # map the similar index of multiple containers so that they can be used just using as single entity.
-        for correct, predicted in zip(labels.astype(int), predictions):
+        for correct, predicted in zip(labels.astype(int), np.round(predictions)):
             matrix[correct][predicted] += 1
         return matrix
 
@@ -34,39 +34,54 @@ class LeastSquares(Classifier):
     def linear_distance(self, data, data_mean):
         return data - data_mean
 
-    def mean(self, X_test):
-        return np.mean(X_test)
-
     def fit(self, x, y):
         self.x = x
         self.y = y
 
-    def predict(self, x_test, y_test):
+    def predict(self, x_test):
         predictions = []
         distances_x = []
         distances_x_squared = []
         distances_y = []
         distances_xy = []
-        x_mean = self.linear_distance(self.x)
-        y_mean = self.linear_distance(self.y)
+        x_mean = np.mean(self.x)
+        y_mean = np.mean(self.y)
 
         for sample in x_test:
             delta_x = self.linear_distance(sample, x_mean)
             distances_x.append(delta_x)
-            distances_x_squared.append(delta_x ** 2)
+            delta_x_squared = delta_x ** 2
+            distances_x_squared.append(delta_x_squared)
 
             delta_y = self.linear_distance(sample, y_mean)
             distances_y.append(delta_y)
-            distances_xy.append(delta_y * distances_x[sample])
+            distances_xy.append(delta_y * delta_x_squared)
 
-        # y = b0 + slope * x
+        # y = intercept + slope * x
         slope = np.sum(distances_xy) / np.sum(distances_x_squared)
         intercept = y_mean - slope * x_mean
 
-        predictions += [sample]
+        for sample in x_test:
+            predictions.append(intercept + slope * sample)
 
         print('Linear regression complete')
         return predictions
+
+
+def show_numbers(X):
+    num_samples = 90
+    # Generates a random sample from a given 1-D array
+    indices = np.random.choice(range(len(X)), num_samples)
+    sample_digits = X[indices]
+
+    fig = plt.figure(figsize=(20, 6))
+
+    for i in range(num_samples):
+        ax = plt.subplot(6, 15, i + 1)
+        img = 255 - sample_digits[i].reshape((16, 16))
+        plt.axis('off')
+        plt.imshow(img, cmap='gray')
+    plt.show()
 
 
 training_data = np.array(pd.read_csv('./datasets/zip.train', sep=' ', header=None, engine='python'))
@@ -78,6 +93,8 @@ x_test, y_test = test_data[:,1:], test_data[:,0]
 model = LeastSquares()
 model.fit(x_train, y_train)
 
-predictions = model.predict(x_test, y_test)
+predictions = model.predict(x_test)
 
 print(model.confusion_matrix(y_test, predictions))
+misclassified = x_test[(predictions != y_test)]
+show_numbers(misclassified)

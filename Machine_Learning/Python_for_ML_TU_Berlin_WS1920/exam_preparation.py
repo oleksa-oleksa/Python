bbbaf0d02f73dc9350d0ff6488f3b3be5c31e033
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-11-15 14:28:43 +0100

    started exam preparation for Python for Machine Learning course at TU Berlin plotting task

--- Machine_Learning/Python_for_ML_TU_Berlin_WS1920/exam_preparation.py
+++ Machine_Learning/Python_for_ML_TU_Berlin_WS1920/exam_preparation.py
@@ -34,9 +34,45 @@ C = np.arange(100, -101, -10).reshape((7, 3))
 #print(C)
 
 D = np.array([1, 2, 3])**2
-print(D)
+#print(D)
 D = D[:, None]
 print("D:", D)
 #D = D.reshape((3))
 #print(D)
 print(0.5*A + D)
+
+D2 = dict([(0, 1), ('1', 2), ('2', 6), (3, 6), (.1, 10)])
+print(D2)
+res = 0
+for k in D2:
+    if type(k) == str:
+        res += int(k)
+        #print(int(k))
+    elif type(k) == int:
+        res += D2[k]
+        #print(D2[k])
+    else:
+        res /= len(D2)
+        print("else")
+        #print(len(D2))
+    print("k:", k)
+    print("res:", res)
+
+
+class Bernoulli():
+    def fit(self, X):
+        self.D = {1: 'Yes', 0: 'No'}
+        conv = lambda x: 1 if x == 'H' else 0
+        res = [conv(x) for x in X]
+        self.p = sum(res) / len(res)
+    def info(self):
+        dec = False
+        if self.p < 0.6 and self.p > 0.4:
+            dec = True
+        return f"The coin is fair: {self.D[int(dec)]} with p value equal {self.p:.3f}"
+
+
+B = Bernoulli()
+B.fit(['H', 'H', 'H', 'T', 'H', 'T', 'T', 'H', 'T', 'T'])
+print(B.info())
+

efd0f466a5682a7c3b66ff8a7e1281f54d4d8e0a
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-11-28 11:12:48 +0100

    IT Security Beuth Substitution ciper text decoder

---
+++ IT_Security_Beuth_MasterMediaInformatik/01_substitution_cipher_decode.py
@@ -0,0 +1,6 @@
+"""
+This tool solves monoalphabetic substitution ciphers, also known as cryptograms.
+These are ciphers where each letter of the clear text is replaced by a corresponding
+letter of the cipher alphabet.
+"""
+

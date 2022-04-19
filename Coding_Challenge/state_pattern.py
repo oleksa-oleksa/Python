2b086779450c76e9943c9fa79e0063e2d5e6a8c0
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-04-19 16:43:53 +0200

    adding state pattern from Python book I am reading now

--- Coding_Challenge/state_pattern.py
+++ Coding_Challenge/state_pattern.py
@@ -17,3 +17,111 @@ class Node:
         else:
             return self.tag_name
 
+
+class Parser:
+    def __init__(self, parse_string):
+        self.parse_string = parse_string
+        self.root = None
+        self.current_node = None
+        self.state = FirstTag()
+
+    def process(self, remaining_string):
+        remaining = self.state.process(remaining_string, self)
+        if remaining:
+            self.process(remaining)
+
+    def start(self):
+        self.process(self.parse_string)
+
+
+class FirstTag:
+    """
+    The FirstTag state will switch to ChildNode,
+    which is responsible for deciding which of
+    the other three states to switch to; when those states are finished,
+    they'll switch back to ChildNode.
+    """
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find('<')
+        i_end_tag = remaining_string.find('>')
+        tag_name = remaining_string[i_start_tag + 1:i_end_tag]
+        root = Node(tag_name)
+        parser.root = parser.current_node = root
+        parser.state = ChildNode()
+        return remaining_string[i_end_tag + 1:]
+
+
+class ChildNode:
+    def process(self, remaining_string, parser):
+        stripped = remaining_string.strip()
+        if stripped.startswith("</"):
+            parser.state = CloseTag()
+        elif stripped.startswith("<"):
+            parser.state = OpenTag()
+        else:
+            parser.state = TextNode()
+
+        return stripped
+
+
+class OpenTag:
+    """
+    The OpenTag state is similar to the FirstTag state,
+    except that it adds the newly created node to the
+    previous current_node object's children and sets
+    it as the new current_node. It places the processor
+    back in the ChildNode state before continuing:
+    """
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find('<')
+        i_end_tag = remaining_string.find('>')
+        tag_name = remaining_string[i_start_tag + 1:i_end_tag]
+        node = Node(tag_name, parser.current_node)
+        parser.current_node.children.append(node)
+        parser.current_node = node
+        parser.state = ChildNode()
+        return remaining_string[i_end_tag + 1:]
+
+
+class CloseTag:
+    """
+    CloseTag basically does the opposite;
+    it sets the parser's current_node back
+    to the parent node so any further children
+    in the outside tag can be added to it:
+    """
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find('<')
+        i_end_tag = remaining_string.find('>')
+        assert remaining_string[i_start_tag + 1] == "/"
+        tag_name = remaining_string[i_start_tag + 2:i_end_tag]
+        assert tag_name == parser.current_node.tag_name
+        parser.current_node = parser.current_node.parent
+        parser.state = ChildNode()
+        return remaining_string[i_end_tag + 1:].strip()
+
+
+class TextNode:
+    """
+    TextNode state very simply extracts the text before
+    the next close tag and sets it as a value on the current node:
+    """
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find('<')
+        text = remaining_string[:i_start_tag]
+        parser.current_node.text = text
+        parser.state = ChildNode()
+        return remaining_string[i_start_tag:]
+
+
+if __name__ == "__main__":
+    import sys
+    with open(sys.argv[1]) as file:
+        contents = file.read()
+        p = Parser(contents)
+        p.start()
+        nodes = [p.root]
+        while nodes:
+            node = nodes.pop(0)
+            print(node)
+            nodes = node.children + nodes

9ebd94b39c131149d47feb33f12d3bda983458e5
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-13 16:29:27 +0200

    firstBadVersion

--- Coding_Challenge/algorithms.py
+++ Coding_Challenge/algorithms.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def search(self, nums: List[int], target: int) -> int:
+def search(nums: List[int], target: int) -> int:
     """
     704. Binary Search
 
@@ -21,7 +21,7 @@ def search(self, nums: List[int], target: int) -> int:
     right_idx = len(nums)
     mid_idx = int((left_idx + right_idx) / 2)
     
-    while (right_idx != left_idx and mid_idx >= left_idx and mid_idx <= right_idx ):
+    while (right_idx != left_idx and mid_idx >= left_idx and mid_idx <= right_idx):
         
         mid_idx = int((left_idx + right_idx) / 2)
         print(mid_idx)
@@ -36,3 +36,36 @@ def search(self, nums: List[int], target: int) -> int:
             
     return -1
     
+def firstBadVersion(n: int) -> int:
+    """
+    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, 
+    which causes all the following ones to be bad.
+    You are given an API bool isBadVersion(version) which returns whether version is bad. 
+    Implement a function to find the first bad version. You should minimize the number of calls to the API.
+    Input: n = 5, bad = 4
+    Output: 4
+    Explanation:
+    call isBadVersion(3) -> false
+    call isBadVersion(5) -> true
+    call isBadVersion(4) -> true
+    Then 4 is the first bad version.
+
+    # The isBadVersion API is already defined for you.
+    # def isBadVersion(version: int) -> bool:
+    """
+    if n== 1:
+        return (1 if isBadVersion(n) else -1)
+        
+    low = 1
+    high = n
+    while(low <= high):
+        mid = (low+high)//2
+        
+        if isBadVersion(mid) and not isBadVersion(mid-1):
+            return mid
+        elif isBadVersion(mid) and isBadVersion(mid-1):
+            high = mid-1
+        elif not isBadVersion(mid):
+            low = mid+1
+    return -1
+ 

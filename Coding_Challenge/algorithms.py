1116627af01b17a4595ef80d949443695fe5e216
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-14 15:04:24 +0200

    added gitattributes to exclude Jyputer from language statistics

--- Coding_Challenge/algorithms.py
+++ Coding_Challenge/algorithms.py
@@ -38,6 +38,7 @@ def search(nums: List[int], target: int) -> int:
     
 def firstBadVersion(n: int) -> int:
     """
+    278. First Bad Version
     Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, 
     which causes all the following ones to be bad.
     You are given an API bool isBadVersion(version) which returns whether version is bad. 
